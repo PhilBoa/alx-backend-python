183974b6e37ab56d4989c0373d132aa76a55dfa8
PhilBoa <philicbrefo98@gmail.com>
2023-12-07 20:04:15 +0000

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Module to perform arithmetic operations."""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two floats and returns their sum."""
+    return a + b
