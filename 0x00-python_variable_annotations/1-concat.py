fbdbe8aeb253665f975c4035c6719b97923afa7d
PhilBoa <philicbrefo98@gmail.com>
2023-12-07 20:06:36 +0000

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Module to perform concatenation for two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenate two strings and returns one string."""
+    return str1 + str2
