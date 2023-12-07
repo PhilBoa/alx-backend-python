55cc22265631d7236e45259831657a02c49ec4ee
PhilBoa <philicbrefo98@gmail.com>
2023-12-07 20:19:09 +0000

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Module that takes a list input_list of floats as argument and returns their sum as a float."""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Takes a list of floats and return the sum as a float."""
+    return sum(input_list)
